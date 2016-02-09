8a7a2fe96f4293cd967c6ed714e0e719d8d3113f
Karan K. Budhraja <karanb1@umbc.edu>
2016-02-09 12:52:37 -0500

    added flipping of data

--- compareAdversaryUtility.py
+++ compareAdversaryUtility.py
@@ -1,8 +1,12 @@
 import numpy
 import matplotlib.pyplot as plt
 
-binarySearchData = numpy.loadtxt("fullCycle_BinarySearch.txt", delimiter=",", skiprows=1)
+binarySearchData = numpy.loadtxt("learningTest.txt", delimiter=",", skiprows=1)
+binarySearchData = numpy.flipud(binarySearchData)
 plt.plot(binarySearchData[:,-2])
-learningWithBinarySearchData = numpy.loadtxt("fullCycle_LearningWithBinarySearch.txt", delimiter=",", skiprows=1)
+
+learningWithBinarySearchData = numpy.loadtxt("learningTest_.txt", delimiter=",", skiprows=1)
+learningWithBinarySearchData = numpy.flipud(learningWithBinarySearchData)
 plt.plot(learningWithBinarySearchData[:,-2])
+
 plt.show()
